8c924797955b2cd381963c32d573a8a3dc12b24d
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-09 23:43:41 +0100

    Update cw05/Zad5c

--- ppj/cw05/Zad5c.java
+++ ppj/cw05/Zad5c.java
@@ -4,9 +4,8 @@ public class Zad5c {
 
     int a, b;
     for (a=b=(1<<11)-1; a<1<<22; a<<=1,b>>=1)
-      System.out.println(
-        Integer.toString((1<<11)|(a^b)&((1<<11)-1),2)
-          .substring(1).replace("1"," "));
+      System.out.println(Integer.toString(
+        (1<<11)|(a^b)&((1<<11)-1),2).replace("1"," "));
 
   }
 
