111152d8effdd0a84a149c2cab4492f5c07dfd8f
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-09 14:50:59 +0100

    Update cw05/Zad5a

--- ppj/cw05/Zad5a.java
+++ ppj/cw05/Zad5a.java
@@ -5,7 +5,7 @@ public class Zad5a {
         int m = 3;
 
         for (int j = -m; j <= m; j++) {
-            for (int i = 0; i <= Math.abs(j)+m; i++)
+            for (int i = 0; i-m <= Math.abs(j); i++)
                 System.out.print(m-i > Math.abs(j) ? " " : "*");
             System.out.println();
         }
