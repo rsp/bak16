e8ba515166dbc634ce45c6b2d6e7e7d2cdda700b
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-11 00:52:41 +0100

    Add ZadM08

---
+++ ppj/cw06pd/ZadM08.java
@@ -0,0 +1,18 @@
+public class ZadM08 {
+
+    public static void main(String[] args) {
+
+        rect(4,  8, 'A');
+        rect(2, 12, 'B');
+        rect(4, 16, 'C');
+
+    }
+
+    static void rect(int a, int b, char c) {
+
+        for (int i = 0; i < a*b; i++)
+            System.out.format(i%b == b-1 ? "%s\n" : "%s", c);
+
+    }
+
+}
