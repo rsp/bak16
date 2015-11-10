a663fc6fa2d3f5ad15115eb83890547b8d04378e
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-11 00:21:38 +0100

    Add ZadM04

---
+++ ppj/cw06pd/ZadM04.java
@@ -0,0 +1,23 @@
+public class ZadM04 {
+
+    public static void main(String[] args) {
+
+        int[] t1 = {1,2,3,4,5,6,7,8},
+              t2 = {10,20,30,40,50,60,70,80},
+              t3 = {100,200,300,400,500,600,700};
+        
+        int[][] tt = {t1, t2, t3};
+
+        printInts2D(tt);
+
+    }
+
+    static void printInts2D(int[][] t) {
+        for (int j = 0; j < t.length; j++) {
+            for (int i = 0; i < t[j].length; i++)
+                System.out.format("%4d", t[j][i]);
+            System.out.println();
+        }
+    }
+
+}
