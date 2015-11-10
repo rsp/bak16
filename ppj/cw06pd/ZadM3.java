8c2a1c66ea1d701aa403b8c0bfa377777da2528a
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-10 23:57:16 +0100

    Add ZadM3

---
+++ ppj/cw06pd/ZadM3.java
@@ -0,0 +1,21 @@
+public class ZadM3 {
+
+    public static void main(String[] args) {
+
+        int[] t1 = {1,2,3},
+              t2 = {10,-20,30,-40,50},
+              t3 = {5,50,500};
+        
+        printInts(t1);
+        printInts(t2);
+        printInts(t3);
+
+    }
+
+    static void printInts(int[] t) {
+        for (int i = 0; i < t.length; i++)
+            System.out.format("%4d", t[i]);
+        System.out.println();
+    }
+
+}
