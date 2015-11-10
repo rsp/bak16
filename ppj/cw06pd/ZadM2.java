d85821fc5ae93ee326d8fb8ae15546d45ef12363
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-10 23:48:05 +0100

    Add ZadM2

---
+++ ppj/cw06pd/ZadM2.java
@@ -0,0 +1,18 @@
+public class ZadM2 {
+
+    public static void main(String[] args) {
+
+        for (double r = 0; r < 3; r++) {
+            System.out.format("r = %.2f, P = %.2f\n", r, pole(r));
+        }
+
+    }
+
+    static double pole(double r) {
+        int x=0;
+        for (int i = 0; i < 4e6; i++)
+            x += Math.pow(Math.random(),2) + Math.pow(Math.random(),2) <= 1 ? 1 : 0;
+        return r*r*x/1e6;
+    }
+
+}
