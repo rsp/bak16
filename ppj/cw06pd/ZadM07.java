0b4a4d560414017d31fae9f48e62f85876cd2971
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-11 00:46:17 +0100

    Add ZadM07

---
+++ ppj/cw06pd/ZadM07.java
@@ -0,0 +1,25 @@
+public class ZadM07 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 20; i+=5)
+            printInts(randInts(i));
+
+    }
+
+    static int[] randInts(int l) {
+
+        int[] t = new int[l];
+        for (int i = 0; i < l; i++)
+            t[i] = (int)(Math.random() * 10);
+        return t;
+
+    }
+
+    static void printInts(int[] t) {
+        for (int i = 0; i < t.length; i++)
+            System.out.format("%3d", t[i]);
+        System.out.println();
+    }
+
+}
