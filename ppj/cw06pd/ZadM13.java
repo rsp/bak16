9534c32e394fa901f62c6db56740dfd61b56b457
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-11 18:56:59 +0100

    Add ZadM13

--- ppj/cw06pd/ZadM13.java
+++ ppj/cw06pd/ZadM13.java
@@ -2,11 +2,24 @@ public class ZadM13 {
 
     public static void main(String[] args) {
 
-        char[] t = {'1', 'a', '0', 'x', '9'};
+        char[][] t = {{'1', 'a', '0', 'x', '9'},
+                      {'1', '2', '3', '4', '5', '6'},
+                      {'0', '0', '0', '0', '0', '0', '.'}};
 
-        for (int i = 0; i < t.length; i++)
-            System.out.format("[%s] %s\n", t[i], test(t[i]));
+        for (int i = 0; i < t.length; i++) {
+            for (int j = 0; j < t[i].length; j++)
+                System.out.print(t[i][j]);
+            System.out.format(" %s\n", test(t[i]));
+        }
 
+    }
+
+    static boolean test(char[] t) {
+        return test(t, 0);
+    }
+
+    static boolean test(char[] t, int i) {
+        return i == t.length ? true : test(t[i]) && test(t, i+1);
     }
 
     static boolean test(char c) {
