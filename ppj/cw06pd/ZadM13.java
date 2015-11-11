353afa66641c87054ab846a012fb3fe1066b265d
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-11 01:34:57 +0100

    Add ZadM10

---
+++ ppj/cw06pd/ZadM13.java
@@ -0,0 +1,16 @@
+public class ZadM13 {
+
+    public static void main(String[] args) {
+
+        char[] t = {'1', 'a', '0', 'x', '9'};
+
+        for (int i = 0; i < t.length; i++)
+            System.out.format("[%s] %s\n", t[i], test(t[i]));
+
+    }
+
+    static boolean test(char c) {
+        return (c+"").matches("\\d");
+    }
+
+}
