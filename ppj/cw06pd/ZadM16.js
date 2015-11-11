7bb0d881add2a3cf34757a8a06ab57883bc510be
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-11 19:49:32 +0100

    Add ZadM16.js

---
+++ ppj/cw06pd/ZadM16.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+var t = [0,1,2,3,4,5,6,7,8,9];
+
+printInts(t);
+
+var p = even(t);
+
+printInts(p);
+
+function even(t) {
+    return t.filter(x => x % 2 == 0);
+}
+    
+function printInts(t) {
+    console.log(t.join(' '));
+}
