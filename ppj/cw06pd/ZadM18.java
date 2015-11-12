881451a3e1ab26d5bbe17fc0d6d31bc4cde7ece0
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-12 10:12:04 +0100

    Add ZadM18

---
+++ ppj/cw06pd/ZadM18.java
@@ -0,0 +1,21 @@
+public class ZadM18 {
+
+    static boolean palindrom(String s) {
+        String t = s.replaceAll("\\W", "").toLowerCase();
+        for (int i = 0, l = t.length(); i < l/2; i++)
+            if (t.charAt(i) != t.charAt(l-i-1)) return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String[] t = {"Anna",
+                      "Zakopane na pokaz.",
+                      "Zakopane nie na pokaz.",
+                      "Aibofobia"};
+        for (int i = 0; i < t.length; i++)
+            System.out.format("\"%s\" %s\n", t[i], palindrom(t[i]));
+
+    }
+
+}
