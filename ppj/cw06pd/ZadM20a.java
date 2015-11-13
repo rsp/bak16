88a7afc19bd4d628677bd608736e00dbacad94ee
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-13 15:03:20 +0100

    Update ZadM20a

--- ppj/cw06pd/ZadM20a.java
+++ ppj/cw06pd/ZadM20a.java
@@ -50,12 +50,9 @@ public class ZadM20a {
 
     public static void main(String[] args) {
 
-        int[][] t = {{0,1,2,3,4,5,6,7,8,9},
-                     {8,2,0,6,4,3,5,9,1,7},
-                     {9,7,1,2,8,6,5,3,4,0}};
+        int[] t = {0,1,2,3,4,5,6,7,8,9};
 
-        for (int i = 0; i < t.length; i++)
-            eosort(t[i]);
+        eosort(t);
 
     }
 
