ab7f8dafe9b1cb5ae3e1eaeb60d4e8d7c710b557
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-13 14:53:54 +0100

    Add ZadM20a

---
+++ ppj/cw06pd/ZadM20a.js
@@ -0,0 +1,22 @@
+function printInts(t) {
+    console.log(t.join(' '));
+}
+
+function eosort(t) {
+
+    printInts(t);
+
+    var b = [t.filter(x => x % 2 == 0).sort(),
+             t.filter(x => x % 2).sort().reverse()],
+        i = [0,0], r = t.map(x => b[x%2][i[x%2]++]);
+
+    printInts(r);
+    console.log();
+
+}
+
+var t = [[0,1,2,3,4,5,6,7,8,9],
+         [8,2,0,6,4,3,5,9,1,7],
+         [9,7,1,2,8,6,5,3,4,0]];
+
+t.forEach(x => eosort(x));
