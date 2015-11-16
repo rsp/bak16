6016d7b8b0c64797640d43472fbf35be6cd4f633
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-16 11:20:00 +0100

    Update ZadM12.java

--- ppj/cw06pd/ZadM12.java
+++ ppj/cw06pd/ZadM12.java
@@ -2,15 +2,38 @@ public class ZadM12 {
 
     public static void main(String[] args) {
 
+        System.out.print("ASCII: ");
         for (int i = 0; i < 50; i++)
-            System.out.print(rand());
+            System.out.print(randASCII());
+
+        System.out.print("\nWiecej: ");
+        for (int i = 0; i < 50; i++)
+            System.out.print(randMore());
+
+        System.out.print("\nWszystkie: ");
+        for (int i = 0; i < 50; i++)
+            System.out.print(randAll());
 
         System.out.println();
 
     }
 
-    static char rand() {
+    static char randASCII() {
         return (char)('a'+(Math.random()*('z'-'a'+1)));
+    }
+
+     static char randMore() {
+        int i = 0;
+        while (!Character.isLowerCase(i))
+            i = (int)(Math.random()*0x250);
+        return (char)i;
+    }
+
+    static char randAll() {
+        int i = 0;
+        while (!Character.isLowerCase(i))
+            i = (int)(Math.random()*Character.MAX_VALUE);
+        return (char)i;
     }
 
 }
