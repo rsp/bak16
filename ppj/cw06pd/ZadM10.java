ce1764ad63362f4e5d05893a6f6d29f75d347225
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-16 10:47:39 +0100

    Update ZadM10

--- ppj/cw06pd/ZadM10.java
+++ ppj/cw06pd/ZadM10.java
@@ -2,7 +2,7 @@ public class ZadM10 {
 
     public static void main(String[] args) {
 
-        char[] t = {'a', ' ', ',', '-', 'x'};
+        char[] t = {'a', ' ', ',', '-', 'x', '\n'};
 
         for (int i = 0; i < t.length; i++)
             System.out.format("[%s] %s\n", t[i], test(t[i]));
