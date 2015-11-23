ae915a9067b7fa61bc48eddb6ef16bd4097ba995
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-23 12:18:41 +0100

    Update cw08/Zad1

--- ppj/cw08/Zad1.java
+++ ppj/cw08/Zad1.java
@@ -4,12 +4,48 @@ public class Zad1 {
         return n <= 2 ? n : fib(n-1) + fib(n-2);
     }
 
+    // taki zapis:
+
+    //     return WARUNEK ? A : B;
+
+    // oznacza to samo co:
+
+    //     if (WARUNEK)
+    //         return A;
+    //     else
+    //         return B;
+
+    // zamiast tego można napisać:
+
+    // static int fib(int n) {
+    //     if (n <= 2)
+    //         return n;
+    //     else
+    //         return fib(n-1) + fib(n-2);
+    // }
+
     public static void main(String[] args) {
 
         int n = 2;
         for (int i = 0; i < 20; i++)
-            System.out.format("%3d %5d\n", i, fib(i));
+            System.out.format( "%3d %5d\n", i, fib(i) );
 
     }
+
+    // System.out.format to taki mądrzejszy System.out.print
+    // mówi się mu najpierw jak ma wyświetlić - np. "%3d %5d\n"
+    // a potem co ma wyswietlić - np. a, b
+
+    // "%3d" oznacza liczbę dziesiętną (d) na szerokość 3 znaków
+    // spacja to spacja
+    // "%5d" oznacza liczbę dziesiętną (d) na szerokość 5 znaków
+    // "\n" to znak nowej linnii - jak System.out.println();
+
+    // Liczby wyświetlane w ten sposób są równane do prawej i
+    // ładnie wyglądają. Zamiast tego można napisać prościej:
+
+        // int n = 2;
+        // for (int i = 0; i < 20; i++)
+        //     System.out.println( i + " " + fib(i) );
 
 }
