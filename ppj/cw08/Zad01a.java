c71b5330cf99360df5f234d1c9445dffa002a479
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-24 00:54:25 +0100

    Add cw08/Zad01a

---
+++ ppj/cw08/Zad01a.java
@@ -0,0 +1,14 @@
+public class Zad01a {
+
+    static long fib(long n) {
+        return n <= 2 ? n : fib(n-1) + fib(n-2);
+    }
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 50; i++)
+            System.out.format( "%3d: %5d\n", i, fib(i) );
+
+    }
+
+}
