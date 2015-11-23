9071b07a1367447b62f02980f5560d6a9e1532d6
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-23 12:05:32 +0100

    Update cw08/Zad3

--- ppj/cw08/Zad3.java
+++ ppj/cw08/Zad3.java
@@ -2,32 +2,56 @@ public class Zad3 {
 
     public static void main(String[] args) {
 
-        int    i = 10000;
-        float  f = 1.11111111111111111111f;
-        char   c = 1000;
-        byte   b = 100;
-        double d = 2.22222222222222222222;
+        char   c = 'A';  // znak
+        byte   b = 111;  // bajt (liczba całkowita od -128 do 127)
+        int    i = 2222; // int (liczba całkowita od ok. minus do plus 2 miliardy)
+        float  f = 3.33333333333333333333f; // ułamek z "f" na końcu oznacza float
+        double d = 4.44444444444444444444;  // ułamek bez "f" na końcu to double
 
+        // double się nazywa double bo zajmuje w pamięci 2 razy tyle co float
+        // i ma dzięki temu większą precyzję (więcej miejsc po przecinku)
+
+        // tworzymy obiekt klasy MethodCourier po to, żebyśmy mogli wywoływać metody:
         MethodCourier m = new MethodCourier();
+
+        // Obiekty klasy MethodCourier (zdefiniowanej niżej) mają
+        // dwie metody o tej samej nazwie ale o różnych typach argumentów (int i float).
+        // To która wersja się uruchomi zależy od tego z jakim argumentem ją wywołamy.
+        // Jeśli wywołamy z argumentem int to uruchomi się wersja int.
+        // Jeśli wywołamy z argumentem int to uruchomi się wersja z int;
+        // Jeśli wywołamy z czymś innym niż int lub float (np. byte albo double)
+        // to wtedy Java spróbuje czy ten nasz typ da się zamienić bez straty na
+        // jeden z typow obsługiwanych przez nasze metody (czyli na int lub float)
+        // i jeśli się da to uruchomi odpowiednią wersję metody a jeśli się nie da
+        // to będziemy mieli błąd o potencjalnej utracie precyzji.
 
         // To wszystko skonwertuje się automatycznie do int lub float
         // a następnie uruchomi się odpowiednia metoda dla int lub float:
         System.out.println("--- Automatyczne konwersje:");
-        System.out.print("int:\t");    m.setValue( i );
-        System.out.print("float:\t");  m.setValue( f );
-        System.out.print("char:\t");   m.setValue( c );
-        System.out.print("byte:\t");   m.setValue( b );
+
+        System.out.print("wywolana metoda(int)\t");
+        m.setValue( i );
+
+        System.out.print("wywolana metoda(float)\t");
+        m.setValue( f );
+
+        System.out.print("wywolana metoda(char)\t");
+        m.setValue( c );
+
+        System.out.print("wywolana metoda(byte)\t");
+        m.setValue( b );
 
         // To nie zadziała bo double nie można skonwertować do
         // ani int ani float bez potencjalnej straty precyzji:
-        // System.out.print("double:\t"); m.setValue(d);
+        // System.out.print("wywolana metoda(double)\t");
+        // m.setValue(d);
 
         // Można zrobić ręcznie konwersję z double do float:
         System.out.println("--- Ręczne konwersje:");
-        System.out.print("(float)double:\t"); m.setValue( (float)d );
+        System.out.print("wywolana metoda((float)double)\t"); m.setValue( (float)d );
 
         // Można też zrobić ręcznie konwersję z double do int:
-        System.out.print("(int)double:\t"); m.setValue( (int)d );
+        System.out.print("wywolana metoda((int)double)\t"); m.setValue( (int)d );
 
         // To jest double bez konwersji i bez utraty precyzji:
         System.out.println("--- Oryginalna wartość double:");
@@ -37,10 +61,15 @@ public class Zad3 {
 }
 
 class MethodCourier {
+
+    // metoda setValue w wersji obsługującej argument typu int:
     void setValue (int n) {
-        System.out.println("int:\t" + n);
+        System.out.println("wybrana metoda(int):\t" + n);
     }
+
+    // metoda setValue w wersji obsługującej argument typu float:
     void setValue (float n) {
-        System.out.println("float:\t" + n);
+        System.out.println("wybrana metoda(float):\t" + n);
     }
+
 }
