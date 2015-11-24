171f5bae320d1e311a4b445f126e434c63872f77
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-24 16:49:34 +0100

    Update cw08pd/Zad04

--- ppj/cw08pd/Zad04.java
+++ ppj/cw08pd/Zad04.java
@@ -52,19 +52,19 @@ public class Zad04 {
 
         int x = 10, m = 3;
 
-        System.out.println("fun1:");
+        System.out.println("--------- fun1 ---------");
         for (int i = 0; i <= m; i++)
             System.out.format( "%3d:%20d\n", i, fun1(x, i) );
 
-        System.out.println("fun2:");
+        System.out.println("--------- fun2 ---------");
         for (int i = 0; i <= m; i++)
             System.out.format( "%3d:%20d\n", i, fun2(x, i) );
 
-        System.out.println("fun3:");
+        System.out.println("--------- fun3 ---------");
         for (int i = 0; i <= m; i++)
             System.out.format( "%3d:%20d\n", i, fun3(x, i) );
 
-        System.out.println("fun4:");
+        System.out.println("--------- fun4 ---------");
         for (int i = 0; i <= m; i++)
             System.out.format( "%3d:%20d\n", i, fun4(x, i) );
 
