4c600097f69c6f461204b8d1767e3e7205937c39
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-24 01:06:44 +0100

    Add cw08/Zad01b

---
+++ ppj/cw08/Zad01b.java
@@ -0,0 +1,18 @@
+public class Zad01b {
+
+    static long[] m  = new long[100];
+
+    static long fib(int n) {
+        return n <= 2 ? n
+             : m[n] > 0 ? m[n]
+             : (m[n] = fib(n-1) + fib(n-2));
+    }
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 50; i++)
+            System.out.format( "%3d: %5d\n", i, fib(i) );
+
+    }
+
+}
