51a8a61a0e11c2eafeb3be3e670d9ba9f79fde32
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-30 17:21:35 +0100

    Add ZadO01

---
+++ ppj/cw08pd/ZadO01.java
@@ -0,0 +1,16 @@
+class Point {
+
+    public double x;
+    public double y;
+
+}
+
+public class ZadO01 {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+
+    }
+
+}
