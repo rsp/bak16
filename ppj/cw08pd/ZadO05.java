95c1e34736e4c97caeafc92686b71082613905dc
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-11-30 17:32:30 +0100

    Add ZadO05

---
+++ ppj/cw08pd/ZadO05.java
@@ -0,0 +1,30 @@
+class Point {
+
+    public double x = 2;
+    public double y = 3;
+
+    Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
+
+public class ZadO05 {
+
+    public static void main(String[] args) {
+
+        Point p1 = new Point(), p2 = new Point();
+
+        System.out.format("p1: %f, %f\n", p1.x, p1.y);
+        System.out.format("p2: %f, %f\n", p2.x, p2.y);
+
+        p2.x = 10;
+        p2.y = 15;
+
+        System.out.format("p1: %f, %f\n", p1.x, p1.y);
+        System.out.format("p2: %f, %f\n", p2.x, p2.y);
+
+    }
+
+}
