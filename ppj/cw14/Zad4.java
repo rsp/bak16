5bfffac10a32c31fc92f7f487ec0513b67fe7512
Rafał Pocztarski <r.pocztarski@gmail.com>
2016-01-20 18:12:15 +0100

    Update cw14/Zad4

--- ppj/cw14/Zad4.java
+++ ppj/cw14/Zad4.java
@@ -3,16 +3,16 @@ import java.util.regex.Matcher;
 
 public class Zad4 {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		String s = "Wies w Polsce polozona ... 1998-1999 ... ...";
+        String s = "Wies w Polsce polozona ... 1998-1999 ... ...";
 
-		int np = 0;
-		Matcher pm = Pattern.compile("\\d+-\\d+").matcher(s);
-		while (pm.find()) np++;
+        int np = 0;
+        Matcher pm = Pattern.compile("\\d+-\\d+").matcher(s);
+        while (pm.find()) np++;
 
-		System.out.println("pary liczb: " + np);
+        System.out.println("pary liczb: " + np);
 
-	}
+    }
 
 }
