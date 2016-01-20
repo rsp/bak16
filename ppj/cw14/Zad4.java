f0f3145d4a3e3c24888feba9277e27fd72442059
Rafał Pocztarski <r.pocztarski@gmail.com>
2016-01-20 17:17:03 +0100

    Add cw14/Zad4

---
+++ ppj/cw14/Zad4.java
@@ -0,0 +1,18 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Zad4 {
+
+	public static void main(String[] args) {
+
+		String s = "Wies w Polsce polozona ... 1998-1999 ... ...";
+
+		int np = 0;
+		Matcher pm = Pattern.compile("\\d+-\\d+").matcher(s);
+		while (pm.find()) np++;
+
+		System.out.println("pary liczb: " + np);
+
+	}
+
+}
