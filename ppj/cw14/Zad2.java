3b5fdcfa089caf23372e75d4cb5524b7e9aa7fff
Rafał Pocztarski <r.pocztarski@gmail.com>
2016-01-20 18:15:56 +0100

    Add cw14/Zad2

---
+++ ppj/cw14/Zad2.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.FileReader;
+import java.util.StringTokenizer;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Zad2 {
+
+    public static void main(String[] args) throws Exception {
+
+        StringBuffer b = new StringBuffer();
+        FileReader r = new FileReader(new File("bson-wiki.txt"));
+
+        int c;
+        while ((c = r.read()) > -1) {
+            b.append((char)c);
+        }
+
+        int wc = 0, nc = 0;
+
+        Matcher wm = Pattern.compile("\\b\\w+\\b").matcher(b);
+        while (wm.find()) wc++;
+
+        Matcher nm = Pattern.compile("\\b\\d+\\b").matcher(b);
+        while (nm.find()) nc++;
+
+        System.out.println("słowa:  " + wc);
+        System.out.println("liczby: " + nc);
+
+    }
+
+}
