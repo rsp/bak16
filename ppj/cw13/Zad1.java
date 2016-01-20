6e24c0640721c4adc751ead8b397a91cadc6904f
Rafał Pocztarski <r.pocztarski@gmail.com>
2016-01-20 19:09:07 +0100

    Update cw13/Zad1

--- ppj/cw13/Zad1.java
+++ ppj/cw13/Zad1.java
@@ -1,32 +1,32 @@
 class List {
-	int head;
-	List rest;
-	List(int i, List r) {
-		head = i;
-		rest = r;
-	}
+    int head;
+    List rest;
+    List(int i, List r) {
+        head = i;
+        rest = r;
+    }
 }
 
 class Stack {
-	List s;
-	void push(int i) {
-		s = new List(i, s);
-	}
-	int pop() {
-		int i = s.head;
-		s = s.rest;
-		return i;
-	}
+    List s;
+    void push(int i) {
+        s = new List(i, s);
+    }
+    int pop() {
+        int i = s.head;
+        s = s.rest;
+        return i;
+    }
 }
 
 public class Zad1 {
 
-	public static void main(String[] args) {
-		Stack s = new Stack();
-		s.push(10);
-		s.push(20);
-		System.out.println(s.pop());
-		System.out.println(s.pop());
-	}
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(10);
+        s.push(20);
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+    }
 
 }
