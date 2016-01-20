e3db6e015a35b673616ab2fb24d194247f3edba9
Rafał Pocztarski <r.pocztarski@gmail.com>
2016-01-20 18:11:56 +0100

    Update cw14/Zad3

--- ppj/cw14/Zad3.java
+++ ppj/cw14/Zad3.java
@@ -5,24 +5,24 @@ import java.util.regex.Matcher;
 
 public class Zad3 {
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		StringBuffer b = new StringBuffer();
-		FileReader r = new FileReader(new File("bson-wiki.txt"));
+        StringBuffer b = new StringBuffer();
+        FileReader r = new FileReader(new File("bson-wiki.txt"));
 
-		int c;
-		while ((c = r.read()) > -1) {
-			b.append((char)c);
-		}
+        int c;
+        while ((c = r.read()) > -1) {
+            b.append((char)c);
+        }
 
-		int ob = 0;
-		Matcher bm = Pattern.compile("[\\[\\]]").matcher(b);
-		while (bm.find()) {
-			if (bm.group().equals("[")) ob++;
-			else if (--ob < 0) break;
-		}
-		System.out.println("nawiasy " + (ob == 0 ? "ok" : "nie ok"));
+        int ob = 0;
+        Matcher bm = Pattern.compile("[\\[\\]]").matcher(b);
+        while (bm.find()) {
+            if (bm.group().equals("[")) ob++;
+            else if (--ob < 0) break;
+        }
+        System.out.println("nawiasy " + (ob == 0 ? "ok" : "nie ok"));
 
-	}
+    }
 
 }
