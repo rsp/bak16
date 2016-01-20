b96db50279b6673fc16ec2e71949a8ecfb08fc50
Rafał Pocztarski <r.pocztarski@gmail.com>
2016-01-20 17:33:15 +0100

    Add cw14/Zad3

---
+++ ppj/cw14/Zad3.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.FileReader;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Zad3 {
+
+	public static void main(String[] args) throws Exception {
+
+		StringBuffer b = new StringBuffer();
+		FileReader r = new FileReader(new File("bson.wiki"));
+
+		int c;
+		while ((c = r.read()) > -1) {
+			b.append((char)c);
+		}
+
+		int bc = 0, ob = 0;
+		Matcher bm = Pattern.compile("[\\[\\]]").matcher(b);
+		while (bm.find()) {
+			if (bm.group().equals("[")) ob++;
+			else if (--ob < 0) break;
+		}
+		System.out.println("nawiasy " + (ob == 0 ? "ok" : "nie ok"));
+
+	}
+
+}
